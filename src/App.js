9a507e4a362e3072db21469ae77fa0c2f8d4cfac
conde_adra <condeadramane82@gmail.com>
2024-06-10 23:43:43 -0400

    Get set up and fetch quotes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import {loader} from "react-feather";
 import "./App.css";
 
 function App() {
+
+  const [quotes, setQuotes] =  useState([]);
+  const [loading, setLoading] = useState(false); 
+
   const quotesUrl =
     "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
-
+  
+    const fetchQuotes = async () => {
+    try{
+      setLoading( true);
+      const request = await fetch(quotesUrl)
+      const resp = await request.json();
+      setQuotes(resp);
+    }catch(e){
+         console.log("THere is an error " + e)
+    }
+    setLoading(false);
+  }
+  useEffect( () => {
+      fetchQuotes();
+  }, [])
   return (
     <div className='App'>
       <Header />
-      <main></main>
+      <main>
+        {loading ? <loader/>: JSON.stringify(quotes)}
+      </main>
       <Footer />
     </div>
   );
